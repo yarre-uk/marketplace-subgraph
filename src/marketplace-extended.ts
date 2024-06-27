13ee01267cc770560788e0c7b58f4199f37d05dc
Yarre <yaroslav.syvukha@gmail.com>
2024-06-27 12:37:10 +0200

    feat: deploy v0.0.5

--- src/marketplace-extended.ts
+++ src/marketplace-extended.ts
@@ -1,42 +1,26 @@
 import {
-  Initialized as InitializedEvent,
   OrderCreated as OrderCreatedEvent,
   OrderProcessed as OrderProcessedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  RoleAdminChanged as RoleAdminChangedEvent,
-  RoleGranted as RoleGrantedEvent,
-  RoleRevoked as RoleRevokedEvent
 } from "../generated/MarketplaceExtended/MarketplaceExtended"
 import {
-  Initialized,
-  OrderCreated,
-  OrderProcessed,
-  OwnershipTransferred,
-  RoleAdminChanged,
-  RoleGranted,
-  RoleRevoked
+  Order
 } from "../generated/schema"
 
-export function handleInitialized(event: InitializedEvent): void {
-  let entity = new Initialized(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.version = event.params.version
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+enum OrderStatus {
+  Created,
+  Processed,
+  Canceled,
 }
 
 export function handleOrderCreated(event: OrderCreatedEvent): void {
-  let entity = new OrderCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
+  let entity = new Order(
+    event.params.id
   )
+
   entity.MarketplaceExtended_id = event.params.id
   entity.sender = event.params.sender
   entity.orderType = event.params.orderType
+  entity.orderStatus = OrderStatus.Created;
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
@@ -46,77 +30,12 @@ export function handleOrderCreated(event: OrderCreatedEvent): void {
 }
 
 export function handleOrderProcessed(event: OrderProcessedEvent): void {
-  let entity = new OrderProcessed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.MarketplaceExtended_id = event.params.id
-  entity.executer = event.params.executer
-  entity.state = event.params.state
+  let entity = Order.load(event.params.id)
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (entity == null) {
+    throw new Error("Order not found");
+  }
 
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
-  let entity = new RoleAdminChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.previousAdminRole = event.params.previousAdminRole
-  entity.newAdminRole = event.params.newAdminRole
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleGranted(event: RoleGrantedEvent): void {
-  let entity = new RoleGranted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleRevoked(event: RoleRevokedEvent): void {
-  let entity = new RoleRevoked(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  entity.orderStatus = event.params.state as OrderStatus;
+  entity.save();
 }
